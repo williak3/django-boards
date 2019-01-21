b7a26fff74361626a40ae31eb094fe89f8ec0e35
williak3 <kenneth.williams@thermofisher.com>
2019-01-21 11:21:57 -0800

    Initial commit

---
+++ accounts/test/test_password_reset.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import views as auth_views
+from django.core.urlresolvers import reverse
+from django.urls import resolve
+from django.test import TestCase
+
+class PasswordResetDoneTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset_done')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/done/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
---
+++ boards/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Board
+
+admin.site.register(Board)
+
+
+
+# Register your models here.
